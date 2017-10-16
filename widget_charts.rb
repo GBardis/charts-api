96e4a4c7c69ee6b2ee0f2872939f9859d9231596
GBardis <crisolivasavatage@hotmail.com>
2017-10-16 14:46:31 +0300

    add new query param datapoint_count , add faker for modem names , add filter option

--- widget_charts.rb
+++ widget_charts.rb
@@ -3,94 +3,56 @@ require 'sinatra'
 configure { set :server, :puma }
 require 'json'
 require 'byebug'
-#
-# get '/widget_charts/line_chart' do
-#   content_type :json
-#   @data = {}
-#   @array = []
-#   WidgeCharts = Struct.new(:widget)
-#   WidgeChart = Struct.new(:chart_type, :chart_data)
-#   @charts = WidgeCharts.new(
-#     [
-#       WidgeChart.new('line', [
-#                        ['data1', 30, 200, 100, 400, 150, 250],
-#                        ['data2', 50, 20, 10, 40, 15, 25]
-#                      ]).to_h
-#     ]
-#   )
-#   @data = { data: @charts.widget }
-#   @data.to_json
-# end
-# get '/widget_charts/spline_chart' do
-#   content_type :json
-#   @data = {}
-#   @array = []
-#   WidgeCharts = Struct.new(:widget)
-#   WidgeChart = Struct.new(:chart_type, :chart_data)
-#   @charts = WidgeCharts.new(
-#     [
-#       WidgeChart.new('spline', [
-#                        ['data1', 30, 200, 100, 400, 150, 250],
-#                        ['data2', 50, 20, 10, 40, 15, 25]
-#                      ]).to_h
-#     ]
-#   )
-#   @data = { data: @charts.widget }
-#   @data.to_json
-# end
-# get '/widget_charts/bar_chart' do
-#   content_type :json
-#   @data = {}
-#   @array = []
-#   WidgeCharts = Struct.new(:widget)
-#   WidgeChart = Struct.new(:chart_type, :chart_data)
-#   @charts = WidgeCharts.new(
-#     WidgeChart.new('bar', [
-#                      ['data1', 30, 200, 100, 400, 150, 250],
-#                      ['data2', 130, 100, 140, 200, 150, 50]
-#                    ]).to_h
-#   )
-#   @data = { data: @charts.widget }
-#   @data.to_json
-# end
-# get '/widget_charts/pie_chart' do
-#   content_type :json
-#   @data = {}
-#   @array = []
-#   WidgeCharts = Struct.new(:widget)
-#   WidgeChart = Struct.new(:chart_type, :chart_data)
-#   @charts = WidgeCharts.new(
-#     [
-#       WidgeChart.new('pie', [
-#                        ['data1', 30, 200, 100, 400, 150, 250],
-#                        ['data2', 50, 20, 10, 40, 15, 25]
-#                      ]).to_h
-#     ]
-#   )
-#   @data = { data: @charts.widget }
-#   @data.to_json
-# end
+require 'faker'
 
 get '/widget_charts' do
   modem_count = params[:modem].to_i
-  type = params[:type]
-
+  type = params[:chart_type]
+  datapoint_count = params[:datapoint_count].to_i
   content_type :json
   @data = {}
   @array = []
   WidgeCharts = Struct.new(:widget)
-  WidgeChart = Struct.new(:chart_type, :chart_data, :modem_name)
+  WidgeChart = Struct.new(:chart_type, :count, :chart_data, :modem_name, :filters)
   modem_array = []
   modem_names = []
 
-  modem_count.times do
-    modem_array << ["modem#{rand(10)}", rand(600), rand(600), rand(600), rand(600), rand(600), rand(600)]
-    modem_names << 'modem' + rand(modem_count).to_s
+  unless datapoint_count.nil?
+    modem_count.times do |index|
+      modem = Faker::App.unique.name
+      modem_array << [modem.to_s]
+      datapoint_count.times do
+        modem_array.at(index) << rand(600)
+      end
+      modem_names << modem.to_s
+    end
+    filters =
+      [
+        {
+          title: '',
+          filter_type: 'checkbox',
+          collection: [
+            value: '',
+            label: 'All'
+          ]
+        },
+        {
+          title: '',
+          filter_type: 'textbox',
+          collection: [
+            value: datapoint_count,
+            label: 'Datapoints'
+          ]
+        }
+      ]
   end
   @charts = WidgeCharts.new(
     [
       WidgeChart.new(type,
-                     modem_array, modem_names).to_h
+                     modem_count,
+                     modem_array,
+                     modem_names,
+                     filters).to_h
     ]
   )
   @data = { data: @charts.widget }
