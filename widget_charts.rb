e2a30ff47f35a551763a229961ceaf32200bf756
frcake <mpdevcake@gmail.com>
2017-11-09 16:25:22 +0200

    fix several issues

--- widget_charts.rb
+++ widget_charts.rb
@@ -7,135 +7,135 @@ require 'faker'
 require 'date'
 require 'active_support/all'
 
-get '/widget_charts/v1' do
-  content_type :json
-  @data = {}
-  @array = []
-  modem_array = []
-  modem_names = []
-  dates = []
-  modem_names_array = []
-  WidgeCharts = Struct.new(:widget)
-  WidgeChart = Struct.new(:chart_type, :chart_data, :modem_name, :filters)
-  modem_count = 5
-  type = params[:chart_type]
-
-  current_date = Faker::Date.backward(14)
-
-  available_date = Faker::Date.backward(30)
-  date_range = ((current_date + 7) - current_date).to_i
-
-  modem_count.times do |index|
-    modem = Faker::App.name
-    modem_array << [modem.to_s]
-    date_range.times do
-      modem_array.at(index) << rand(600)
-    end
-    modem_names << modem.to_s
-  end
-
-  modem_names.each do |name|
-    hash = { name: name }
-    modem_names_array << hash
-  end
-
-  filters =
-  [
-    {
-      title: 'date_filter',
-      type: 'date',
-      current_start_date: current_date,
-      current_end_date: current_date + 7,
-      available_start: available_date,
-      available_end: available_date + 30
-    },
-    {
-      title: 'modems',
-      type: 'multi-select',
-      collection: modem_names_array
-    }
-  ]
-
-  @charts = WidgeCharts.new(
-    [
-      WidgeChart.new(type,
-        modem_array,
-        modem_names,
-      filters).to_h
-    ]
-  )
-  @data = { data: @charts.widget }
-  @data.to_json
-end
-
-get '/widget_charts' do
-  modem_names = []
-
-  params[:modem].each do |modem_name|
-    modem_names << modem_name
-  end
-  modem_count = modem_names.count
-
-  start_date = Date.parse(params[:startDate])
-  end_date = Date.parse(params[:endDate])
-
-  date_range = (end_date - start_date).to_i
-  type = params[:chart_type]
-
-  content_type :json
-
-  @data = {}
-  @array = []
-  WidgeCharts = Struct.new(:widget)
-  WidgeChart = Struct.new(:chart_type, :chart_data, :modem_name, :filters)
-  modem_array = []
-  dates = []
-  modem_names_array = []
-
-  modem_names.each do |name|
-    modem_array << [name]
-  end
-
-  modem_count.times do |index|
-    date_range.times do
-      modem_array.at(index) << rand(600)
-    end
-  end
-
-  date_range.times do
-    hash = { value: Faker::Date.between(end_date, start_date).strftime('%d-%m-%Y') }
-    dates << hash
-  end
-
-  modem_names.each do |name|
-    hash = { name: name }
-    modem_names_array << hash
-  end
-
-  filters = [
-    {
-      title: 'date_filter',
-      type: 'date',
-      collection: dates
-    },
-    {
-      title: 'modems',
-      type: 'multi-select',
-      collection: modem_names_array
-    }
-  ]
-
-  @charts = WidgeCharts.new(
-    [
-      WidgeChart.new(type,
-        modem_array,
-        modem_names,
-      filters).to_h
-    ]
-  )
-  @data = { data: @charts.widget }
-  @data.to_json
-end
+# get '/widget_charts/v1' do
+#   content_type :json
+#   @data = {}
+#   @array = []
+#   modem_array = []
+#   modem_names = []
+#   dates = []
+#   modem_names_array = []
+#   WidgeCharts = Struct.new(:widget)
+#   WidgeChart = Struct.new(:chart_type, :chart_data, :modem_name, :filters)
+#   modem_count = 5
+#   type = params[:chart_type]
+#
+#   current_date = Faker::Date.backward(14)
+#
+#   available_date = Faker::Date.backward(30)
+#   date_range = ((current_date + 7) - current_date).to_i
+#
+#   modem_count.times do |index|
+#     modem = Faker::App.name
+#     modem_array << [modem.to_s]
+#     date_range.times do
+#       modem_array.at(index) << rand(600)
+#     end
+#     modem_names << modem.to_s
+#   end
+#
+#   modem_names.each do |name|
+#     hash = { name: name }
+#     modem_names_array << hash
+#   end
+#
+#   filters =
+#   [
+#     {
+#       title: 'date_filter',
+#       type: 'date',
+#       current_start_date: current_date,
+#       current_end_date: current_date + 7,
+#       available_start: available_date,
+#       available_end: available_date + 30
+#     },
+#     {
+#       title: 'modems',
+#       type: 'multi-select',
+#       collection: modem_names_array
+#     }
+#   ]
+#
+#   @charts = WidgeCharts.new(
+#     [
+#       WidgeChart.new(type,
+#         modem_array,
+#         modem_names,
+#       filters).to_h
+#     ]
+#   )
+#   @data = { data: @charts.widget }
+#   @data.to_json
+# end
+#
+# get '/widget_charts' do
+#   modem_names = []
+#
+#   params[:modem].each do |modem_name|
+#     modem_names << modem_name
+#   end
+#   modem_count = modem_names.count
+#
+#   start_date = Date.parse(params[:startDate])
+#   end_date = Date.parse(params[:endDate])
+#
+#   date_range = (end_date - start_date).to_i
+#   type = params[:chart_type]
+#
+#   content_type :json
+#
+#   @data = {}
+#   @array = []
+#   WidgeCharts = Struct.new(:widget)
+#   WidgeChart = Struct.new(:chart_type, :chart_data, :modem_name, :filters)
+#   modem_array = []
+#   dates = []
+#   modem_names_array = []
+#
+#   modem_names.each do |name|
+#     modem_array << [name]
+#   end
+#
+#   modem_count.times do |index|
+#     date_range.times do
+#       modem_array.at(index) << rand(600)
+#     end
+#   end
+#
+#   date_range.times do
+#     hash = { value: Faker::Date.between(end_date, start_date).strftime('%d-%m-%Y') }
+#     dates << hash
+#   end
+#
+#   modem_names.each do |name|
+#     hash = { name: name }
+#     modem_names_array << hash
+#   end
+#
+#   filters = [
+#     {
+#       title: 'date_filter',
+#       type: 'date',
+#       collection: dates
+#     },
+#     {
+#       title: 'modems',
+#       type: 'multi-select',
+#       collection: modem_names_array
+#     }
+#   ]
+#
+#   @charts = WidgeCharts.new(
+#     [
+#       WidgeChart.new(type,
+#         modem_array,
+#         modem_names,
+#       filters).to_h
+#     ]
+#   )
+#   @data = { data: @charts.widget }
+#   @data.to_json
+# end
 
 get '/widget_charts/v2' do
   content_type :json
@@ -201,7 +201,7 @@ get '/widget_charts/v2' do
     end
   else
     modem_params_array.each_with_index do |modem, index|
-      modem_name<< modem
+      modem_name<< { name: modem }
       modem_array << [modem]
       date_range.times do
         modem_array.at(index) << rand(600)
