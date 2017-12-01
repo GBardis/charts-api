784a6a4429f34b066761b616f8730a62fe7c87f9
mpdev <mpdevcake@gmail.com>
2017-12-01 14:24:07 +0200

    Changes route to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/widget_charts/v4', to: 'application#v4'
+  post '/widget_charts/v4', to: 'application#v4'
   # put '/widget_charts/v4', to: 'application#v4'
 end
