6be2ddc808571a31d6c2ac7cb75b0c86e25d612d
mpdev <mpdevcake@gmail.com>
2017-12-01 14:29:52 +0200

    Adds v3 and fixes responses

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post '/widget_charts/v4', to: 'application#v4'
+  post '/widget_charts/v3', to: 'application#v3'
   # put '/widget_charts/v4', to: 'application#v4'
 end
