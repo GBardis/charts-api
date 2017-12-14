6cf89936a775a1d3888927197beb8cbd8f30b027
GBardis <crisolivasavatage@hotmail.com>
2017-12-14 10:42:52 +0200

    dasherize param name

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   post '/widget_charts/v3', to: 'application#v3'
   post '/widget_charts/v4', to: 'application#v4'
   post '/widget_charts/v5', to: 'application#v5'
+  post '/widget_charts/v6', to: 'application#v6'
 end
